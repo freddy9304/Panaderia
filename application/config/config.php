994f5ad0d6e7ddf1fbc50796a4b1cfc74c57b4ec
freddy9304 <alfredo-9304@hotmail.com>
2018-11-23 17:28:22 -0600

    cambio de url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-    $config['base_url'] = 'localhost:8080/Panaderia/';
+    $config['base_url'] = 'https://panaderia1.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
